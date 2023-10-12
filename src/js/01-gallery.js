2dc65236e48256a0d1f084015c336cd284b8e64e
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-12 19:43:51 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const listGallery = document.querySelector('.gallery');
+// listGallery.style.listStyle = 'none';
+
+function renderMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+listGallery.insertAdjacentHTML('beforeend', renderMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

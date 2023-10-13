c6bb453bfe4d38af960d9ee1d0ca8438ee64f5e3
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-14 00:41:26 +0300

    add local storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import _ from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const locStorage = {
+  getSrotage(key) {
+    return JSON.parse(localStorage.getItem(key));
+  },
+  setStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+};
+
+form.addEventListener('input', _(formHandler, 500));
+
+function formHandler(event) {
+  const formValue = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+    
+  locStorage.setStorage('feedback-form-state', formValue);
+}

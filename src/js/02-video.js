01a4036c59f931dcbcb9a7aec42f87b0615663f6
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-14 13:30:25 +0300

    03-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,7 @@ import _ from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
+const keyStorage = 'videoplayer-current-time';
 const locStorage = {
   getSrotage(key) {
     return JSON.parse(localStorage.getItem(key));
@@ -12,34 +13,11 @@ const locStorage = {
   },
 };
 
-// player.on('play', function (data) {
-//   data.seconds = 100;
-//   console.log('data play: ', data);
-// });
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-// 'videoplayer-current-time';
 player.on(
   'timeupdate',
   _(function (data) {
-    locStorage.setStorage('videoplayer-current-time', data.seconds);
+    locStorage.setStorage(keyStorage, data.seconds);
   }, 1000)
 );
-player
-  .setCurrentTime(locStorage.getSrotage('videoplayer-current-time'))
-  .then(function (seconds) {
-    // console.log('seconds: ', seconds);
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(locStorage.getSrotage(keyStorage));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,7 @@
 import _ from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const keyStorage = 'feedback-form-state';
 const locStorage = {
   getSrotage(key) {
     return JSON.parse(localStorage.getItem(key));
@@ -11,12 +12,31 @@ const locStorage = {
 };
 
 form.addEventListener('input', _(formHandler, 500));
+form.addEventListener('submit', onSubmit);
 
 function formHandler(event) {
   const formValue = {
     email: form.elements.email.value,
     message: form.elements.message.value,
   };
-    
-  locStorage.setStorage('feedback-form-state', formValue);
+
+  locStorage.setStorage(keyStorage, formValue);
 }
+function onSubmit(event) {
+  event.preventDefault();
+  const bd = locStorage.getSrotage(keyStorage);
+
+  if (bd) {
+    console.log(bd);
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+    localStorage.clear(keyStorage);
+  }
+}
+function fillingForms(bd) {
+  if (bd) {
+    form.elements.email.value = bd.email;
+    form.elements.message.value = bd.message;
+  }
+}
+fillingForms(locStorage.getSrotage(keyStorage));
